fea2e7251f057702e00a8cb7cf154a88c159c3be
Veer Pratap Singh <113500751+veer073@users.noreply.github.com>
2024-08-14 13:49:36 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
- export const host= "http://localhost:5000";
+ // export const host= "http://localhost:5000";
+ export const host= "https://chatengine-backend.onrender.com";
  export const registerRoute= `${host}/api/auth/register`;
  export const loginRoute= `${host}/api/auth/login`;
  export const SetAvatarRoute= `${host}/api/auth/SetAvatar`;
